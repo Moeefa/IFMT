dc472ed633dbd16bce0166f4ff629e02b1fd0f9a
Moeefa <moeefa@protonmail.com>
2023-02-09 17:51:04 -0400

    Exibe em qual tabela o usuário se insere.

--- CalcIMC.java
+++ CalcIMC.java
@@ -10,6 +10,23 @@ public class CalcIMC {
 
         float calc = (float) (number1 / (Math.pow(number2, 2)));
 
-        JOptionPane.showMessageDialog(null, String.format("IMC: %.2f", calc), "Cálculo IMC", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(
+                null,
+                String.format("IMC: %.2f\nVocê está %s", calc,
+                        (calc <= 18.5
+                                ? "abaixo do peso normal"
+                                : calc <= 24.9
+                                        ? "com o peso normal"
+                                        : calc <= 29.9
+                                                ? "acima do peso"
+                                                : calc <= 34.9
+                                                        ? "com obesidade classe I"
+                                                        : calc <= 39.9
+                                                                ? "com obesidade classe II"
+                                                                : calc > 39.9
+                                                                        ? "com obesidade classe III"
+                                                                        : "")),
+                "Cálculo IMC",
+                JOptionPane.PLAIN_MESSAGE);
     }
 }
